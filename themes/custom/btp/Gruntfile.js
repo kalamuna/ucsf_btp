1ae5090e711ddf31d14879d562b17f1e784abde4
Josh Walker <josh@kalamuna.com>
2015-03-24 10:46:51 -0700

    Setting up front end tooling

---
+++ themes/custom/btp/Gruntfile.js
@@ -0,0 +1,128 @@
+module.exports = function(grunt) {
+
+  require('load-grunt-tasks')(grunt);
+
+  var themeJs = [
+        'bower_components/matchHeight/jquery.matchHeight.js',
+        'js/btp.js'
+      ];
+
+  grunt.initConfig({
+
+    concat: {
+      themeJs: {
+        files: {
+          'js/dist/btp.js': themeJs
+        }
+      }
+    },
+    uglify: {
+      options: {
+        sourceMap: false
+      },
+      themeJs: {
+        files: {
+          'js/dist/btp.js': 'js/dist/btp.js'
+        }
+      }
+    },
+    cssmin: {
+      target: {
+        files: {
+          'css/main.css': 'css/main.css'
+        }
+      }
+    },
+    watch: {
+      gruntfile: {
+        files: 'Gruntfile.js',
+        options: {
+          reload: true
+        }
+      },
+      sass: {
+        files: 'scss/**/*.scss',
+        tasks: ['sass:dev', 'kss'],
+        options: {
+          livereload: true
+        }
+      },
+      themeJs: {
+        files: themeJs,
+        tasks: ['concat:themeJs']
+      }
+    },
+    sass: {
+      options: {
+        sourcemap: true,
+        trace: true,
+        includePaths: [
+          'bower_components',
+          'bower_components/bootstrap-sass-official/assets/stylesheets/',
+          'bower_components/fontawesome/scss/'
+        ]
+      },
+      dev: {
+        options: {
+          lineNumbers: true,
+          outputStyle: 'nested'
+        },
+        files: {
+          'css/main.css': 'scss/main.scss',
+        }
+      },
+      dist: {
+        options: {
+          compressed: true
+        },
+        files: {
+          'css/main.css': 'scss/main.scss'
+        }
+      }
+    },
+    jshint: {
+      themeJs: {
+        files: {
+          src: ['js/btp.js']
+        },
+        options: {
+          reporter: require('jshint-stylish'),
+          jshintrc: '.jshintrc'
+        }
+      }
+    },
+    copy: {
+      ksscss: {
+        src: ['css/main.css', 'css/styleguide.css', 'css/admin.css'],
+        dest: 'styleguide/public/'
+      },
+      kssscripts: {
+        src: 'js/dist/**',
+        dest: 'styleguide/assets/'
+      },
+      kssfonts: {
+        src: 'fonts/**',
+        dest: 'styleguide/assets/'
+      }
+    },
+    kss: {
+      options: {
+        mask: '*.scss',
+        template: './kss-template'
+      },
+      dist: {
+        files: {
+          './styleguide': ['./scss']
+        }
+      }
+    }
+
+  });
+
+  grunt.registerTask("productionbuild", ['newer:concat', 'uglify', 'sass:dist', 'cssmin', 'guide']);
+  grunt.registerTask("devbuild", ['newer:concat', 'sass:dev', 'guide']);
+  grunt.registerTask("develop", ['devbuild','watch']);
+  grunt.registerTask("default", ['develop']);
+  return grunt.registerTask('guide', ['kss', 'copy:ksscss', 'copy:kssfonts', 'copy:kssscripts']);
+
+};
--- themes/custom/btp/template.php
+++ themes/custom/btp/template.php
@@ -3,3 +3,15 @@
  * @file 
  * Primary pre/preprocess functions and alterations.
  */ 
+function btp_preprocess_html(&$vars) {
+  global $theme_path;
+  
+  // Include admin.css in admin pages or where admin bits are exposed.
+  $admin_pages = array('admin', 'node', 'user');
+  if (in_array(arg(0), $admin_pages)) {
+    drupal_add_css($theme_path . '/css/specifics/admin.css');
+  }
+
+  $livereload_port = 35729;
+  drupal_add_js("document.write('<script src=\"http://' + (location.host || 'localhost').split(':')[0] + ':$livereload_port/livereload.js?snipver=1\"></' + 'script>')", array('type' => 'inline', 'scope' => 'footer', 'weight' => 9999));
+}
