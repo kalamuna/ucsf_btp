bdea9f3fbd53fa4f32dde221e0dc6ef90d6b4e95
Josh Walker <josh@kalamuna.com>
2015-03-24 17:35:49 -0700

    First stab at theming

--- themes/custom/btp/template.php
+++ themes/custom/btp/template.php
@@ -2,6 +2,10 @@
 /** 
  * @file 
  * Primary pre/preprocess functions and alterations.
+ */
+ 
+/**
+ * Implements hook_preprocess_html
  */ 
 function btp_preprocess_html(&$vars) {
   global $theme_path;
@@ -14,4 +18,23 @@ function btp_preprocess_html(&$vars) {
 
   $livereload_port = 35729;
   drupal_add_js("document.write('<script src=\"http://' + (location.host || 'localhost').split(':')[0] + ':$livereload_port/livereload.js?snipver=1\"></' + 'script>')", array('type' => 'inline', 'scope' => 'footer', 'weight' => 9999));
+}
+
+/**
+ * Implements hook_preprocess_page
+ */ 
+function btp_preprocess_page(&$vars) {
+  // For some reason the navbar has a container class which conflicts with the
+  // div directly inside it which also has a container class. Let's remove it.
+  foreach ($vars['navbar_classes_array'] as $i => $val) {
+    if ($val== 'container') {
+      unset($vars['navbar_classes_array'][$i]);
+    }
+  }
+  
+  // Build the search block for printing directly in the page template.
+  $vars['search_block'] = module_invoke('search', 'block_view', 'search');
+  
+  // Change the size of the content column.
+  $vars['content_column_class'] = ' class="col-sm-8"';
 }
